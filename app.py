3b325e4fcb4b2d71223e59486f1d65d1bc78f7aa
Nat0110tan <tan00250@umn.edu>
2023-02-09 22:55:47 -0600

    idk what's going on

--- app.py
+++ app.py
@@ -1,40 +1,70 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
+from flask import Flask, render_template, request
+import config
+from exts import db
+from models import UserModel
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-DATABASE_URL="postgresql://hw_user:TzaO8qnbLogp5M3XwsLwK0EpTz45Z9Mf@dpg-cfht36la499bfu2gfnng-a.oregon-postgres.render.com/hw"
-db = SQLAlchemy(app)
+app.config.from_object(config)
+db.init_app(app)
+migrate = Migrate(app, db)
 
-class Cat(db.Model):
-  __tablename__ = "cat"
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  #varchar, null=0
-  name = db.Column(db.String(100), nullable=False)
-  password = db.Column(db.String(100), nullable=False)
 
-@app.route("/cat/update")
-def update_user():
-  catt = Cat.query.filter_by(name="meme").first()
-  # 这里的first返回的是一个user对象而不是query类数组
-  catt.password = "123456" #已经是在这个数据库里面操作了,不用add
-  db.session.commit()
-  return "Successfully changed"
+from flask import Flask, request, render_template, jsonify
+app = Flask(__name__)
 
-with app.app_context():
-  db.create_all()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def hello():
-  return "hi"
+@app.route('/survey', methods=['GET', 'POST'])
+def survey():
+    if request.method == 'POST':
+        response = {
+            'text_input': request.form['text_input'],
+            'radio_buttons': request.form['radio_buttons'],
+            'select_box': request.form['select_box'],
+            'checkbox': request.form['checkbox'],
+            'textarea': request.form['textarea']
+        }
+  
+        return render_template('thanks.html')
+    return render_template('survey.html')
 
-@app.route("/add/<string:n>")
-def helloo(n):
-  cat = Cat(name=n,password="123")
-  db.session.add(cat)
-  db.session.commit()
-  return "Successfully added a new cat!"
+@app.route('/decline')
+def decline():
+    return render_template('decline.html')
+
+@app.route('/thanks', methods=['POST'])
+def thanks():
+    return render_template('thanks.html')
+
+
+
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+# @app.route("/survey", methods=["GET", "POST"])
+# def survey():
+#     if request.method == "POST":
+#         name = request.form["name"]
+#         radio = request.form["radio"]
+#         select = request.form["select"]
+#         checkbox = "on" if "checkbox" in request.form else None
+#         textarea = request.form["textarea"] if checkbox else None
+#         return render_template("thanks.html", name=name, radio=radio, select=select, checkbox=checkbox, textarea=textarea)
+#     return render_template("survey.html")
+
+# @app.route("/decline")
+# def decline():
+#     return "Thanks anyway!"
+
+# @app.route("/thanks")
+# def thanks():
+#     return render_template("thanks.html")
+
 
 
 if __name__ == '__main__':
