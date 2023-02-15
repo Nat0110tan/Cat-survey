40dfd5489b5eb03e40ead7be97ff325300f79baf
Nat0110tan <tan00250@umn.edu>
2023-02-15 00:46:26 -0600

    yeaaaa

--- app.py
+++ app.py
@@ -6,24 +6,22 @@ import json,os
 
 
 
-app = Flask(__name__)
-# app.config.from_object(config)
-app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get("DATABASE_URL")
-DATABASE_URL="postgresql://hw_user:TzaO8qnbLogp5M3XwsLwK0EpTz45Z9Mf@dpg-cfht36la499bfu2gfnng-a.oregon-postgres.render.com/hw"
 
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+DATABASE_URL="postgresql://hw_user:TzaO8qnbLogp5M3XwsLwK0EpTz45Z9Mf@dpg-cfht36la499bfu2gfnng-a.oregon-postgres.render.com/hw"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-with app.app_context():
- db.create_all()
 
 class Response(db.Model):
     __tablename__ = "useraccount"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), nullable=False)
-    gender = db.Column(db.String(10), nullable=False)
+    gender = db.Column(db.String(10), nullable=True)
     color = db.Column(db.String(10), nullable=True)
-    cats = db.Column(db.Boolean, nullable=True)
+    cats = db.Column(db.String(80), nullable=True)
     reason = db.Column(db.String(500), nullable=True)
     join_time = db.Column(db.DateTime, default=datetime.now)
 
@@ -37,7 +35,7 @@ def survey():
         name = request.form['name']
         gender = request.form['gender']
         color = request.form['color']
-        cats = 'no' in request.form
+        cats = ','.join(request.form.getlist('cats'))
         reason = request.form.get('reason', '')
         join_time = request.form.get('join_time')
         response = Response(name=name, gender=gender, color=color, cats=cats, reason=reason, join_time=join_time)
@@ -76,8 +74,56 @@ def get_results():
 
 
 @app.route('/admin/summary')
-def summary():
-    return render_template('summary.html')
+def admin_summary():
+    responses = Response.query.all()
+
+    # process the responses to generate data for charts
+    color_counts = {}
+    gender_counts = {}
+    cats_counts = {}
+    reason_answers = []
+
+    for response in responses:
+        color = response.color
+        gender = response.gender
+        cats = response.cats.split(',')
+        reason = response.reason
+
+        if color:
+            if color not in color_counts:
+                color_counts[color] = 0
+            color_counts[color] += 1
+
+        if gender:
+            if gender not in gender_counts:
+                gender_counts[gender] = 0
+            gender_counts[gender] += 1
+
+        if cats:
+            for cat in cats:
+                if cat not in cats_counts:
+                    cats_counts[cat] = 0
+                cats_counts[cat] += 1
+
+        if reason:
+            reason_answers.append(reason)
+
+    # generate data for charts
+    color_data = {
+        'labels': list(color_counts.keys()),
+        'data': list(color_counts.values())
+    }
+    gender_data = {
+        'labels': list(gender_counts.keys()),
+        'data': list(gender_counts.values())
+    }
+    cats_data = {
+        'labels': list(cats_counts.keys()),
+        'data': list(cats_counts.values())
+    }
+
+    # pass the processed data to the template
+    return render_template('admin/summary.html',responses=responses,color_data=color_data,gender_data=gender_data,cats_data=cats_data,reason_answers=reason_answers)
 
 if __name__ == '__main__':
   app.run()
